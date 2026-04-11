be3e34652e8bf2b6f9407bd54c23ca7b22d678b0
Adarsh Nayak <adarshpnayak006@gmail.com>
2026-04-12 00:44:36 +0530

    Merge remote changes

--- models/cycle_gan_E2_model.py
+++ models/cycle_gan_E2_model.py
@@ -3,7 +3,6 @@ from .cycle_gan_model import CycleGANModel as BaseCycleGANModel
 
 
 class CycleGANE2Model(BaseCycleGANModel):
-    # Discriminator (LSGAN)
     def backward_D_basic(self, netD, real, fake):
         pred_real = netD(real)
         loss_D_real = torch.mean((pred_real - 1) ** 2)
@@ -16,29 +15,31 @@ class CycleGANE2Model(BaseCycleGANModel):
 
         return loss_D
 
-    # Generator (LSGAN)
     def backward_G(self):
-
-        # ---- GAN LOSS (LSGAN) ----
+        # GAN LOSS (LSGAN)
         pred_fake_B = self.netD_A(self.fake_B)
         self.loss_G_A = 0.5 * torch.mean((pred_fake_B - 1) ** 2)
 
         pred_fake_A = self.netD_B(self.fake_A)
         self.loss_G_B = 0.5 * torch.mean((pred_fake_A - 1) ** 2)
 
-        # ---- CYCLE LOSS (UNCHANGED) ----
+        # CYCLE LOSS
         self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * self.opt.lambda_A
         self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * self.opt.lambda_B
 
-        # ---- IDENTITY LOSS (UNCHANGED) ----
         if self.opt.lambda_identity > 0:
+
+            self.idt_A = self.netG_A(self.real_B)
+            self.idt_B = self.netG_B(self.real_A)
+
             self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * self.opt.lambda_B * self.opt.lambda_identity
             self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * self.opt.lambda_A * self.opt.lambda_identity
+
         else:
             self.loss_idt_A = 0
             self.loss_idt_B = 0
 
-        # ---- TOTAL LOSS ----
+        # TOTAL LOSS
         self.loss_G = (
             self.loss_G_A +
             self.loss_G_B +
@@ -48,5 +49,4 @@ class CycleGANE2Model(BaseCycleGANModel):
             self.loss_idt_B
         )
 
-        # ---- BACKWARD (ONLY ONCE) ----
         self.loss_G.backward()
--- models/cycle_gan_E3_model.py
+++ models/cycle_gan_E3_model.py
@@ -3,6 +3,9 @@ import torch.autograd as autograd
 from .cycle_gan_model import CycleGANModel as BaseCycleGANModel
 
 
+# -------------------------
+# Gradient Penalty
+# -------------------------
 def compute_gp(D, real, fake):
     alpha = torch.rand(real.size(0), 1, 1, 1).to(real.device)
     interpolates = (alpha * real + (1 - alpha) * fake).requires_grad_(True)
@@ -23,28 +26,54 @@ def compute_gp(D, real, fake):
 
 class CycleGANE3Model(BaseCycleGANModel):
 
+    # Critic (Discriminator)
     def backward_D_basic(self, netD, real, fake):
+
         pred_real = netD(real)
         pred_fake = netD(fake.detach())
 
-        loss = -torch.mean(pred_real) + torch.mean(pred_fake)
+        # WGAN loss
+        loss = torch.mean(pred_fake) - torch.mean(pred_real)
+
+        # Gradient penalty
         gp = compute_gp(netD, real, fake.detach())
 
-        self.loss_D = loss + 10 * gp
-        self.loss_D.backward()
-        return self.loss_D
+        loss_D = loss + 10 * gp
+        loss_D.backward()
 
+        return loss_D
+
+
+    # Generator
     def backward_G(self):
-        super().backward_G()
 
-        pred_fake = self.netD_A(self.fake_B)
-        self.loss_G_A = -torch.mean(pred_fake)
+        # GAN LOSS (WGAN)
+        pred_fake_B = self.netD_A(self.fake_B)
+        self.loss_G_A = -torch.mean(pred_fake_B)
 
-        pred_fake = self.netD_B(self.fake_A)
-        self.loss_G_B = -torch.mean(pred_fake)
+        pred_fake_A = self.netD_B(self.fake_A)
+        self.loss_G_B = -torch.mean(pred_fake_A)
 
-        self.loss_G = self.loss_G_A + self.loss_G_B + \
-                      self.loss_cycle_A + self.loss_cycle_B + \
-                      self.loss_idt_A + self.loss_idt_B
+        self.loss_cycle_A = self.criterionCycle(self.rec_A, self.real_A) * self.opt.lambda_A
+        self.loss_cycle_B = self.criterionCycle(self.rec_B, self.real_B) * self.opt.lambda_B
+
+        if self.opt.lambda_identity > 0:
+            self.idt_A = self.netG_A(self.real_B)
+            self.idt_B = self.netG_B(self.real_A)
+
+            self.loss_idt_A = self.criterionIdt(self.idt_A, self.real_B) * self.opt.lambda_B * self.opt.lambda_identity
+            self.loss_idt_B = self.criterionIdt(self.idt_B, self.real_A) * self.opt.lambda_A * self.opt.lambda_identity
+        else:
+            self.loss_idt_A = 0
+            self.loss_idt_B = 0
+
+        self.loss_G = (
+            self.loss_G_A +
+            self.loss_G_B +
+            self.loss_cycle_A +
+            self.loss_cycle_B +
+            self.loss_idt_A +
+            self.loss_idt_B
+        )
 
         self.loss_G.backward()
